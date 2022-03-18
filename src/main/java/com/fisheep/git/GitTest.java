68329065dd914f5df9db88ac2dc54753920e45cf
Fi_Sheep <2030588032@qq.com>
2022-03-18 16:30:20 +0800

    master commit2

--- src/main/java/com/fisheep/git/GitTest.java
+++ src/main/java/com/fisheep/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello xiaohe");
         System.out.println("hello xiaohe");
         System.out.println("hello xiaohe4444");
+        System.out.println("master xiaohe4444");
     }
 }
