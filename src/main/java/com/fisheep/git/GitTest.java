eb2ca8d1c00969622cf23149449b99b6f603ae8f
Fi-Sheep <94786194+Fi-Sheep@users.noreply.github.com>
2022-03-18 18:26:41 +0800

    pull commit

--- src/main/java/com/fisheep/git/GitTest.java
+++ src/main/java/com/fisheep/git/GitTest.java
@@ -13,6 +13,6 @@ public class GitTest {
         System.out.println("hello xiaohe4444");
         System.out.println("master xiaohe4444");
         System.out.println("hot-fix xiaohe4444");
-        System.out.println("pull xiaohe4444");
+        System.out.println("aini xiaohe4444");
     }
 }
