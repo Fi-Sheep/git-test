ad5aa7a74e316d7c9b5818df695c7e725e89cb9b
Fi_Sheep <2030588032@qq.com>
2022-03-18 16:16:36 +0800

    first commit

---
+++ src/main/java/com/fisheep/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.fisheep.git;
+
+/**
+ * @author Fi_Sheep
+ * @Description:
+ * @create 2022-03-18 16:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello xiaohe");
+    }
+}
